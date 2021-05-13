4b0aafb248d252e66c82c770d0b8ef1f540fca8f
guregu321 <guchegory@gmail.com>
2021-05-13 23:10:06 +0900

    2310

--- photo.py
+++ photo.py
@@ -34,24 +34,24 @@ def update_image(epd, config):
         # Reshape photo
         if photo_image.height / photo_image.width >= 1.5: # If photo height is larger than the screen
             # Resize
-            height = round(photo_image.height * 264 / photo_image.width)
-            photo_image = photo_image.resize((264, height), Image.LANCZOS)
+            height = round(photo_image.height * 176 / photo_image.width)
+            photo_image = photo_image.resize((176, height), Image.LANCZOS)
             # Crop
-            if height > 176:
-                upper, lower = (height+176)/2, (height-176)/2
-            else:
-                upper, lower = 176, 0
-            photo_image = photo_image.crop((0, lower, 264, upper))
-        else: # If photo width is larger than the screen
-            # Resize
-            width = round(photo_image.width * 176 / photo_image.height)
-            photo_image = photo_image.resize((width, 176), Image.LANCZOS)
-            # Crop
-            if width > 264:
-                upper, lower = (width+264)/2, (width-264)/2
+            if height > 264:
+                upper, lower = (height+264)/2, (height-264)/2
             else:
                 upper, lower = 264, 0
-            photo_image = photo_image.crop((lower, 0, upper, 176))
+            photo_image = photo_image.crop((0, lower, 176, upper))
+        else: # If photo width is larger than the screen
+            # Resize
+            width = round(photo_image.width * 264 / photo_image.height)
+            photo_image = photo_image.resize((width, 264), Image.LANCZOS)
+            # Crop
+            if width > 176:
+                upper, lower = (width+176)/2, (width-176)/2
+            else:
+                upper, lower = 176, 0
+            photo_image = photo_image.crop((lower, 0, upper, 264))
 
 
     if config['display']['orientation'] == 90 or config['display']['orientation'] == 270:
